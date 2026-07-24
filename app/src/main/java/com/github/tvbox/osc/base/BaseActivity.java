3e77f4b51ceab84ce349d3d4fd5120b267900818
happylyc <86012625+happylyc@users.noreply.github.com>
2026-07-24 11:54:57 +0800

    Fix missing newline at end of BaseActivity.java
    Added missing newline at the end of the file.

--- app/src/main/java/com/github/tvbox/osc/base/BaseActivity.java
+++ app/src/main/java/com/github/tvbox/osc/base/BaseActivity.java
@@ -14,6 +14,7 @@ import android.text.InputType;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -482,4 +483,4 @@ public abstract class BaseActivity extends AppCompatActivity implements CustomAd
             getWindow().setBackgroundDrawableResource(R.drawable.app_bg);
         }
     }
-}
+                }
